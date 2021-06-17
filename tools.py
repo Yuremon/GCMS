a7a7f018ad98f1e9e1310d2bcdf9dfefce5bb486
LEVARLET julien <julien.levarlet@telecomnancy.eu>
2021-06-17 11:53:36 +0200

    Amélioration du calcul du bias, maintenant indépendant de la fréquence d'échantillonnage de la zone observée

--- tools.py
+++ tools.py
@@ -1,25 +1,24 @@
 import pandas as pd
 import numpy as np
+from pandas.core.frame import DataFrame
 from scipy.signal import butter, filtfilt
 from sklearn.metrics import mean_squared_error
 from os import listdir
 from os.path import join
 from typing import Tuple
 
-PERIOD = [1, 2, 6, 20] 
-RESAMPLE_MS = [str(p) +'s' for p in PERIOD]
 INTERVAL = [6.01,45.01]
 NOISE_FREQ = None #0.09
 BIAS_FREQ = 0.002
-THRESHOLD = 1.5
-SPIKES = [21.5, 22.5, 38]
-SPIKES_EXPECTED_TIME = [21.5, 22.5, 38]
+THRESHOLD = 3.4
+SPIKES = [22.604, 23.821, 38.661]
+SPIKES_EXPECTED_TIME = [21.5, 23, 38]
 PADDING = 45 # attention la valeur doit être supérieur à 1
-TIMES = [INTERVAL[0], 8, 10, 21.5, 22, 24.3, 26.5, 27.8, 30.5, 31.5, 32, 38, INTERVAL[1]] # dermines les zones sur les quelles il faut être plus ou moins précis
-SECTORS = [[3, 8], [1,5,7], [0,2,4,9, 11], [10, 6]] # indice des zones à échantillonage [[élevé], [moyen], [faible]]
-
-# 21.5 et 22 essayer de faire plus precis (1s) entre 30.5 et 31.5
-# faire en sorte de normaliser pour la taille du pic 1 soit tjr la meme
+PERIOD = [1, 2, 6, 20] 
+RESAMPLE_MS = [str(p) +'s' for p in PERIOD]
+#                   0    1    2   3     4     5     6   7     8   9     10  11  12
+TIMES = [INTERVAL[0], 8.7, 9.4, 21, 21.4, 24.5, 25.5, 28, 29.6, 30, 31.5, 32, 38, INTERVAL[1]] # dermines les zones sur les quelles il faut être plus ou moins précis
+SECTORS = [[3, 9], [1,5,7], [0,2,4, 8,10, 12], [11, 6]] # indice des zones à échantillonage [[très élévé], [élevé], [moyen], [faible]]
 
 def readCSV(path : str)->pd.DataFrame:
     """Création du DataFrame a partir d'un csv."""
@@ -31,31 +30,32 @@ def readCSV(path : str)->pd.DataFrame:
     df['values'] = df['values'].fillna(0) # on enlève les éventuels nan
     return df
 
-def adaptCurve(df : pd.DataFrame)->pd.DataFrame:
+def adaptCurve(df : pd.DataFrame, spikes : list)->pd.DataFrame:
     """Passage en log, normalisation, conversion de l'indice en temps, et resample."""
     df.rename(columns = {df.columns[0]: 'values'}, inplace=True)
     df = df.drop(df[df.index >= INTERVAL[1]].index)
     df = df.drop(df[df.index <= INTERVAL[0]].index)
     # alignement des pics
-    df = alignSpikes(df)
+    df = alignSpikes(df, spikes)
     # passage en log
     df[df['values'] == 0] = 0.01  # pour ne pas avoir de - inf
     df = np.log(df)
     # normalisation
     df = (df - df.mean())/df.std()
-    # Etalonner sur le premier pic ( taille de 10 celui-ci)
+    # Etalonner sur le premier pic (taille de 10 pour celui-ci) 
+    # remarque : attention si la valeur n'est pas précise les pics seront plus grand que prévu
     timeSpike1 = getTimeIndex(df.index, SPIKES_EXPECTED_TIME[0])
-    print(df.iloc[timeSpike1])
     df = df / df.iloc[timeSpike1] * 10
     # re-echantillonnage    
     df = resampleByPart(df)
+    df.index = df.index.total_seconds()/60 # on remet le temps de manière plus exploitable
     return df
 
 def resampleByPart(df : pd.DataFrame) -> pd.DataFrame:
     """Fait un échantillonnage avec une période différente pour chaque partie de la courbe"""
     # sélection des intervalles
     parts = [df[(df.index > TIMES[i-1]) & (df.index <= TIMES[i])] for i in range(1, len(TIMES))] 
-     # conversion de la durée en timedelta pour pouvoir faire un resample
+    # conversion de la durée en timedelta pour pouvoir faire un resample
     for p in range(len(parts)): 
         parts[p].index = pd.to_timedelta(parts[p].index, 'min') 
 
@@ -72,7 +72,7 @@ def resampleByPart(df : pd.DataFrame) -> pd.DataFrame:
         # zone à fréquence d'échantilonnage faible
         if i in SECTORS[3] :
             parts[i] = parts[i].resample(rule=RESAMPLE_MS[3]).max().interpolate(method='polynomial', order=3)
-    
+
     return pd.concat(parts)
 
 def butter_lowpass(cutoff : float, fs : float, order=5)-> Tuple[np.ndarray, np.ndarray]:
@@ -88,38 +88,78 @@ def butter_lowpass_filter(data : pd.DataFrame, cutoff : float, fs : float, order
 
 def substractBias(df : pd.DataFrame)->pd.DataFrame:
     """Filtrage et suppression du bias."""
-    bias = df.rolling(15).min()
     # filtrage
-    bias = computeBiasByPart(bias)
-    #df['bias'] = bias
+    #bias = computeBiasByPart(bias)
+    bias = computeBiasWithResample(df)
+
     if (NOISE_FREQ is None):
-        df['values'] -= bias
+        substractBiasResampled(df, bias)
     else :
         df = butter_lowpass_filter(df['values'], NOISE_FREQ, 1/PERIOD[1]) # attention on perd la hauteur relative entre les pics parfois
-        df -= bias
+        substractBiasResampled(df, bias)
     # seuillage
     df.loc[df["values"] < THRESHOLD, 'values'] = 0
     return df
 
+def computeBiasWithResample(values : pd.DataFrame) -> pd.DataFrame:
+    """calcul du bias avec un resample pour ne pas avoir de fréquences differentes par zones"""
+    # attention parfois une petite baisse sans explication sur les derniers points
+    bias = values.copy()
+    # resample
+    bias.index = pd.to_timedelta(bias.index, 'min') # pour le resample
+    bias = bias.resample(rule=RESAMPLE_MS[0]).pad()    
+    # ajout de valeurs à gauche et à droite pour ne pas avoir d'effet de bord sur le filtrage
+    padding = pd.DataFrame([bias['values'].iloc[0]] * PADDING)    
+    padding.rename(columns = {padding.columns[0]: 'values'}, inplace=True)
+    padding.index = pd.timedelta_range('1s', '5s', periods=PADDING)
+    bias = pd.concat([padding, bias]) # pour compenser le padding du filtre butter à 0 et éliminer l'effet de bord
+    padding['values'] = [bias['values'].iloc[0]] * PADDING
+    bias = pd.concat([bias, padding])
+    bias.index = bias.index.total_seconds()/60 # on remet le temps de manière plus exploitable    
+    # filtrage
+    bias = bias.rolling(30).min()
+    # remplissage des nan introduits
+    bias = bias.fillna(method='ffill') # on retire les Nan en forward et backward (pour être sûr qu'il n'y en ai plus)
+    bias = bias.fillna(method='bfill')    
+    bias['values'] = butter_lowpass_filter(bias['values'].to_numpy(), BIAS_FREQ, 1 / PERIOD[0])
+    return bias
+
+def resizeBias(df : DataFrame, bias: DataFrame) -> np.ndarray:
+    values = [[]] * len(df)
+    for i in range(len(df)):
+        biasValue = bias['values'].iloc[getTimeIndex(bias.index, df.index[i])]
+        values[i] = biasValue
+    return values
+
+def substractBiasResampled(df : DataFrame, bias : pd.DataFrame) -> pd.DataFrame:
+    """Soustraction du bias au DataFrame quand les échelles de temps sont différentes"""
+    values = df['values'].to_numpy()
+    for i in range(len(df)):
+        dfValue = df['values'].iloc[i]
+        biasValue = bias['values'].iloc[getTimeIndex(bias.index, df.index[i])]
+        values[i] = dfValue - biasValue
+    df['values'] = values
+    return df
+
 def computeBiasByPart(values : pd.DataFrame) -> np.ndarray:
-    """calcul du bias pour chaque intervalle"""
+    """calcul du bias pour chaque intervalle, peut induire des problèmes de continuité"""
+    # ajout de valeurs à gauche et à droite pour ne pas avoir d'effet de bord sur le filtrage
     padding = pd.DataFrame([np.NaN] * PADDING)
     padding.index = pd.timedelta_range('1s', '5s', periods=PADDING)
     temp = pd.concat([padding, values]) # pour compenser le padding du filtre butter à 0 et éliminer l'effet de bord
     temp = pd.concat([temp, padding])
     temp = temp.fillna(method='ffill') # on retire les Nan en forward et backward (pour être sûr qu'il n'y en ai plus)
     temp = temp.fillna(method='bfill')
-    temp.index = temp.index.total_seconds()/60
+    #temp.index = temp.index.total_seconds()/60 # mise du temps en seconde pour faciliter les calculs suivant
     indexes = [getTimeIndex(temp.index, i) for i in TIMES]
     indexes[-1] = len(temp) - PADDING # on corrige la position du dernier point au cas où il n'est pas exactement à 45.01
 
-    # sélection des intervalles avec une marge de LENGTH_ADDED de chaque coté de l'intervalle pour éviter les problèmes de continuité
+    # sélection des intervalles avec une marge de PADDING de chaque coté de l'intervalle pour éviter les problèmes de continuité
     bias = temp['values'].to_numpy()
     parts = [bias[indexes[i-1] - PADDING : indexes[i] + PADDING] for i in range(1, len(indexes))] 
     result = [[]] * len(parts)
-    for i in range(len(parts)):  
-        #print('Taille de la partie ', i, ' : ', len(parts[i]) - 2 * PADDING)      
-        #print('partie : ', i, parts[i])
+    # application du filtre
+    for i in range(len(parts)): 
         # zones à fréquence d'échantilonnage très élevée
         if i in SECTORS[0] : 
             result[i] = butter_lowpass_filter(parts[i], BIAS_FREQ, 1/PERIOD[0])[PADDING:-PADDING]
@@ -135,10 +175,13 @@ def computeBiasByPart(values : pd.DataFrame) -> np.ndarray:
     
     return np.concatenate(result)
 
-def readAndAdaptDataFromCSV(path : str) -> pd.DataFrame:
+def readAndAdaptDataFromCSV(path : str, spikes : list = None) -> pd.DataFrame:
     """Lit le fichier et retourne le DataFrame après traitement."""
     df = readCSV(path)
-    df = adaptCurve(df)
+    if spikes is None:
+        df = adaptCurve(df, SPIKES)
+    else: 
+        df = adaptCurve(df, spikes)
     df = substractBias(df)
     return df
 
@@ -147,18 +190,18 @@ def compareCurves(oldDf : pd.DataFrame, newDf : pd.DataFrame) -> float:
     value = mean_squared_error(oldDf, newDf)
     return value
 
-def alignSpikes(df : pd.DataFrame) -> pd.DataFrame:
+def alignSpikes(df : pd.DataFrame, spikes : list) -> pd.DataFrame:
     """Aligne les pics détectés sur les références attention df sera modifié"""
     time = df.index.copy().to_numpy() # pour ne pas modifier df
-    # recherche des indices des pics
-    firstSpikeIndex = getTimeIndex(time, SPIKES[0])
-    if SPIKES[1] is not None:
-        secondSpikeIndex = getTimeIndex(time, SPIKES[1])
-    thirdSpikeIndex = getTimeIndex(time, SPIKES[2])
+    # recherche des indices des pics (second pic pas toujours présent)
+    firstSpikeIndex = getTimeIndex(time, spikes[0])
+    if spikes[1] is not None:
+        secondSpikeIndex = getTimeIndex(time, spikes[1])
+    thirdSpikeIndex = getTimeIndex(time, spikes[2])
     # Alignement du premier pic
     shiftValues(time[:firstSpikeIndex], time[0], SPIKES_EXPECTED_TIME[0])
     # si présent alignement du second si présent et du troisième
-    if SPIKES[1] is not None:
+    if spikes[1] is not None:
         shiftValues(time[firstSpikeIndex:secondSpikeIndex], SPIKES_EXPECTED_TIME[0], SPIKES_EXPECTED_TIME[1])
         shiftValues(time[secondSpikeIndex:thirdSpikeIndex], SPIKES_EXPECTED_TIME[1], SPIKES_EXPECTED_TIME[2])
     else :
@@ -184,6 +227,7 @@ def getTimeIndex(t : np.ndarray, timeValue) -> float:
     return np.argmax(t>=timeValue)
 
 def readAllData(path : str) -> list[pd.DataFrame]:
+    """retourne la liste de DataFrame correspondant à tous les fichiers csv présent dans path"""
     files = [f for f in listdir(path) if f.endswith(".csv") or f.endswith(".CSV")]
     dataList = []
     for file in files:
@@ -191,6 +235,7 @@ def readAllData(path : str) -> list[pd.DataFrame]:
     return dataList
 
 def readListOfData(db : np.ndarray, path : str):
+    """retourne la liste de DataFrame correspondant à tous les fichiers csv de db au chemin path"""
     files = [join(path, file + '.CSV') for file in db]
     dataList = []
     for file in files:
